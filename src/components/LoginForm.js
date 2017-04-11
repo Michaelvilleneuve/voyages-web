20b99963ec8cd17623d67ec4faf53bc0eb10ef56
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-11 14:13:20 +0200

    Improved login form, add state logic

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,38 +5,76 @@ import { bindActionCreators } from 'redux'
 import * as userActions from '../actions/user'
 
 class LoginForm extends Component {
+  state = {
+    password: "",
+    email: ""
+  }
+
+  mailChanged(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  passChanged(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  login() {
+    this.props.login(this.state);
+  }
+
   render() {
     return (
-      <div className="row">
-        <form className="col s12">
-          <div className="row">
-            <div className="input-field col s12">
-              <input type="email" className="validate" />
-              <label htmlFor="email">Email</label>
+    <section className="row">
+      <div className="col s4" />
+      <div className="col s4">
+        <div className="row">
+          <form className="col s12">
+            <h4>Log in</h4>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  type="email"
+                  className="validate"
+                  onChange={this.mailChanged.bind(this)}
+                  value={this.state.email}
+                />
+                <label htmlFor="email">Email</label>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="input-field col s12">
-              <input type="password" className="validate" />
-              <label htmlFor="password">Password</label>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  type="password"
+                  className="validate"
+                  onChange={this.passChanged.bind(this)}
+                  value={this.state.password}
+                />
+                <label htmlFor="password">Password</label>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <button className="waves-effect waves-light btn-large">Envoyer</button>
-          </div>
-        </form>
+            <div className="row">
+              <button
+                type="button"
+                onClick={this.login.bind(this)}
+                className="waves-effect waves-light btn-large"
+              >
+                Envoyer
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    )
+      <div className="col s4" />
+    </section>
+    );
   }
 }
 
-const mapStateToProps = state => ({
-  user: state.user
-})
+const mapStateToProps = state => {
+  return {};
+};
 
-const mapDispatchToProps = dispatch => ({
-  userActions: bindActionCreators(userActions, dispatch)
-})
+const mapDispatchToProps = dispatch => bindActionCreators(userActions, dispatch);
 
 export default connect(
   mapStateToProps,
