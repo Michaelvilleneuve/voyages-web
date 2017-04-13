ea61ba4b0d2c35a64ca4d319a58ba14835ea418b
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-13 12:11:29 +0200

    Add material ui

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,21 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { browserHistory } from 'react-router';
 import isConnected from '../api/connection';
 import './style.css';
 
 export default class App extends Component {
   componentWillMount() {
+    injectTapEventPlugin();
     if (isConnected()) browserHistory.push('/dashboard');
   }
 
   render() {
-    return this.props.children;
+    return (
+      <MuiThemeProvider>
+        {this.props.children}
+      </MuiThemeProvider>
+    );
   }
 }
