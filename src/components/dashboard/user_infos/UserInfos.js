b677c256ae37038a88d7e3ba27cbb125ef227de4
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-13 12:07:58 +0200

    Improved user info page

--- src/components/dashboard/user_infos/UserInfos.js
+++ src/components/dashboard/user_infos/UserInfos.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Add from 'material-ui/svg-icons/content/add';
 import * as userActions from '../../../actions/user';
+import './UserInfos.css';
 
 class UserInfos extends Component {
   componentWillMount() {
@@ -12,8 +16,14 @@ class UserInfos extends Component {
     if (this.props.user === null) return null;
 
     return (
-      <aside>
+      <aside className="user">
+        <Link to="/voyages/new">
+          <FloatingActionButton style={{ position: 'absolute', bottom: 20, right: 30 }} on>
+            <Add />
+          </FloatingActionButton>
+        </Link>
         <h2>{this.props.user.name}</h2>
+        <hr />
       </aside>
     );
   }
