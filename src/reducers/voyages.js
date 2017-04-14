09f8e9327f9033fe8197cf6e0de3f0e280805cb5
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-14 17:13:14 +0200

    New voyage reducer

--- src/reducers/voyages.js
+++ src/reducers/voyages.js
@@ -1,13 +1,16 @@
-import { LIST } from '../types/voyages';
+import { LIST, VALIDATE_FORM } from '../types/voyages';
 
 const initialState = {
-  journeys: []
+  journeys: [],
+  errors: {}
 };
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case LIST:
       return { ...state, journeys: action.payload.journeys };
+    case VALIDATE_FORM:
+      return { ...state, errors: action.payload };
     default:
       return state;
   }
