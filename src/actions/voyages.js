c62b941aec46cf8d1a1ff61cc4b2ef154ae34c4b
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-14 17:12:15 +0200

    Add new voyages actions

--- src/actions/voyages.js
+++ src/actions/voyages.js
@@ -1,4 +1,5 @@
-import { LIST } from '../types/voyages';
+import { browserHistory } from 'react-router';
+import { LIST, VALIDATE_FORM } from '../types/voyages';
 import API from '../api/api';
 
 export function setList() {
@@ -12,4 +13,25 @@ export function setList() {
         });
       });
   };
+}
+
+export function createVoyage(form) {
+  return (dispatch) => {
+    const data = JSON.stringify(form);
+    API.post('/journeys/', data)
+      .then((res) => res.json())
+      .then((journey) => {
+        if ('errors' in journey) {
+          renderFormErrors(journey, dispatch);
+        } else {
+          browserHistory.push('/dashboard');
+        }
+      });
+  };
+}
+
+function renderFormErrors(journey, dispatch) {
+  const errors = {};
+  Object.keys(journey.errors).forEach((e) => errors[e] = journey.errors[e].message);
+  dispatch({ type: VALIDATE_FORM, payload: errors });
 }
