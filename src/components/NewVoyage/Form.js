a0c6e4a38ca88a493b4adc75097220e874464de8
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-14 17:12:40 +0200

    New voyage form

--- src/components/NewVoyage/Form.js
+++ src/components/NewVoyage/Form.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
-import DatePicker from 'material-ui/DatePicker';
-import TextField from 'material-ui/TextField';
-import Toggle from 'material-ui/Toggle';
-import FlatButton from 'material-ui/FlatButton';
+import { DatePicker, TextField, Toggle, FlatButton } from 'material-ui';
+import Place from 'material-ui-geosuggest';
+import * as voyagesActions from '../../actions/voyages';
 
-export default class Form extends Component {
+class Form extends Component {
+  state = {}
+
+  changed(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({ [name]: value });
+  }
+
+  dateChanged(name, date) {
+    this.setState({ [name]: date });
+  }
+
+  placeChanged(place) {
+    if (!place.geometry) return this.setState({ place: '' });
+    const location = place.geometry.location;
+    this.setState({ place: place.formatted_address, lat: location.lat(), lng: location.lng() });
+  }
+
+  submit() {
+    this.props.createVoyage(this.state);
+  }
+
   render() {
     return (
       <div style={{ float: 'left', width: '50%', paddingTop: 30 }}>
@@ -15,24 +40,73 @@ export default class Form extends Component {
             subtitle="C'est le début de l'aventure"
           />
           <CardText>
-            <TextField hintText="Titre" fullWidth />
-            <DatePicker hintText="Début" autoOk fullWidth />
-            <DatePicker hintText="Fin" autoOk fullWidth />
-            <TextField hintText="Description" multiLine fullWidth rows={4} />
+            <TextField
+              name="title"
+              hintText="Titre"
+              fullWidth
+              errorText={this.props.errors.title}
+              onChange={this.changed.bind(this)}
+            />
+            <DatePicker
+              locale="fr"
+              okLabel="OK"
+              cancelLabel="Annuler"
+              DateTimeFormat={global.Intl.DateTimeFormat}
+              errorText={this.props.errors.starts_at}
+              hintText="Début" fullWidth onChange={(e, d) => this.dateChanged('starts_at', d)}
+            />
+            <DatePicker
+              locale="fr"
+              okLabel="OK"
+              DateTimeFormat={global.Intl.DateTimeFormat}
+              cancelLabel="Annuler"
+              errorText={this.props.errors.ends_at}
+              hintText="Fin" fullWidth onChange={(e, d) => this.dateChanged('ends_at', d)}
+            />
+            <Place
+              fullWidth
+              name="place"
+              value={this.state.place}
+              errorText={this.props.errors.place}
+              onChange={this.changed.bind(this)}
+              onPlaceChange={this.placeChanged.bind(this)}
+            />
+            <TextField
+              hintText="Description"
+              name="description"
+              multiLine
+              fullWidth
+              rows={4}
+              errorText={this.props.errors.description}
+              onChange={this.changed.bind(this)}
+            />
           </CardText>
           <CardText>
             <Toggle
-              onToggle={this.handleToggle}
+              onToggle={this.changed.bind(this)}
+              name="public"
               labelPosition="right"
+              errorText={this.props.errors.public}
               label="Rendre mon voyage public"
             />
             <br />
           </CardText>
           <CardActions>
-            <FlatButton label="Enregistrer" primary />
+            <FlatButton
+              label="Enregistrer"
+              primary
+              onClick={this.submit.bind(this)}
+            />
           </CardActions>
         </Card>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators(voyagesActions, dispatch);
+const mapStateToProps = ({ voyages }) => {
+  return { errors: voyages.errors };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
--- src/components/NewVoyage/NewVoyage.js
+++ src/components/NewVoyage/NewVoyage.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import UserInfos from '../dashboard/user_infos/UserInfos';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Arrow from 'material-ui/svg-icons/navigation/arrow-back';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { Link } from 'react-router';
 import Form from './Form';
+import './NewVoyage.css';
 
-export default class Voyage extends Component {
+export default class NewVoyage extends Component {
   render() {
     return (
-      <div>
-        <UserInfos />
-        <Form />
-      </div>
+      <ReactCSSTransitionGroup
+        transitionName="example"
+        transitionAppear
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}
+      >
+        <div className="flex-center new-voyage">
+          <Link to="/dashboard">
+            <FloatingActionButton style={{ position: 'fixed', top: 50, left: 50 }}>
+              <Arrow />
+            </FloatingActionButton>
+          </Link>
+            <Form />
+        </div>
+      </ReactCSSTransitionGroup>
     );
   }
 }
