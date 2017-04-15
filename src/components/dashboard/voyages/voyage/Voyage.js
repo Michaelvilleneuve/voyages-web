b8d69328b84dbfe2c1f6ab4f4bb796ad76d6b633
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-04-15 12:18:23 +0200

    Improved single voyage component

--- src/components/dashboard/voyages/voyage/Voyage.js
+++ src/components/dashboard/voyages/voyage/Voyage.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
+import moment from 'moment';
 import FlatButton from 'material-ui/FlatButton';
 import './Voyage.css';
 
 export default class Voyage extends Component {
+  format(date) {
+    return moment(date).locale('fr').format('LL');
+  }
+
+  renderSubtitle() {
+    const v = this.props.voyage;
+    if (!v.starts_at) return null;
+    return `Du ${this.format(v.starts_at)} au ${this.format(v.ends_at)}`;
+  }
+
+
   render() {
     const voyage = this.props.voyage;
     return (
@@ -12,7 +24,7 @@ export default class Voyage extends Component {
           actAsExpander
           showExpandableButton
           title={voyage.title}
-          subtitle={`Du ${voyage.starts_at} au ${voyage.ends_at}`}
+          subtitle={this.renderSubtitle()}
         />
         <CardText expandable>
           {voyage.description}
